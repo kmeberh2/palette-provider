c586833c236ff6b9c84051887d4b6638f5946bc5
kmeberh2 <32106718+kmeberh2@users.noreply.github.com>
2020-11-23 03:21:50 -0600

    Update BrightnessActivity.java

--- app/src/main/java/com/pstcstest/paletteprovider/BrightnessActivity.java
+++ app/src/main/java/com/pstcstest/paletteprovider/BrightnessActivity.java
@@ -2,10 +2,15 @@ package com.pstcstest.paletteprovider;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+
 
 import com.pstcstest.paletteprovider.R;
 
@@ -16,6 +21,29 @@ public class BrightnessActivity extends AppCompatActivity{
         setContentView(R.layout.activity_brightness);
         ImageButton HomeButtonBrightness = (ImageButton) findViewById(R.id.homebutton_brightness_photo);
         ImageButton BackButtonBrightness = (ImageButton) findViewById(R.id.backbutton_brightness_photo);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.alma465);
+        ImageView imageView = (ImageView) findViewById(R.id.editPhotoImage);
+        imageView.setImageBitmap(bitmap);
+        PictureThread pictureThread = new PictureThread(imageView, bitmap);
+        pictureThread.start();
+
+        SeekBar seekbar = (SeekBar) findViewById(R.id.seekbar);
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                pictureThread.adjustBrightness(seekBar.getProgress()-255);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
         BackButtonBrightness.setOnClickListener(new View.OnClickListener() {
             @Override
